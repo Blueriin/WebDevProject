ca1addec6e6c0b3b2ceb4b2f0b802a575c557be5
Blueriin <Stella.gra@hotmail.fr>
2018-06-22 21:17:29 +0200

    cartes

--- Cartes.php
+++ Cartes.php
@@ -11,6 +11,12 @@
         ?>
     </head>
     <body>
+        <div class="card">
+            <img class="card-img-top" src="images/Photos crêperie/fois-gras.jpg" alt="fois-gras image">
+            <div class="card-body">
+                <button type="button" onclick="" class="btn btn-primary">Les galettes</button>
+            </div>
+        </div>
         
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
