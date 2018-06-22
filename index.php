003b33287fc0dec791b6319edf40851261ee8b8a
Blueriin <Stella.gra@hotmail.fr>
2018-06-22 20:38:19 +0200

    yi

--- index.php
+++ index.php
@@ -39,15 +39,7 @@
                 </div>
             </div>
         </div>
-        <div class="center">
-            <div class="calibri brown contact"><b>
-                <span class="darleston">O</span>uverture
-
-                Cras quis leo nibh. Donec scelerisque justo eget ante laoreet, laoreet pulvinar turpis lobortis. 
-                Maecenas tincidunt eros a ipsum mattis, sed varius lorem mollis. Ut ultrices vel libero a aliquam. Cras ac quam sapien. 
-                Aliquam erat volutpat. Vivamus condimentum sed nunc a tempus. Sed lobortis mattis magna vel pulvinar. 
-                Vestibulum non ultricies ipsum.</div>
-                Vestibulum non ultricies ipsum</div>
+        
           
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
